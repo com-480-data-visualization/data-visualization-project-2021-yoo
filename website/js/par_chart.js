23a9f15d05ca7ac11fdb7ea48069e94ba6051aa7
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2021-06-04 13:24:44 +0200

    read data correct

--- website/js/par_chart.js
+++ website/js/par_chart.js
@@ -143,221 +143,222 @@ function plot_graph(dimensions, position_filter) {
     .attr("transform", function (d, i) { return "translate(" + xscale(i) + ")"; });
 
   console.log('hereeee')
-  d3.json("/website/data/teams_basic_skills.json").then(function (data) {
-    var data_filtered
-    if (position_filter == 'no_filter') {
-      data_filtered = data.filter(x => (x['str_nationality'] == team_par1) | (x['str_nationality'] == team_par2))
-    } else {
-      const filtered_positions = return_valid_positions(position_filter)
-      console.log(filtered_positions)
-      data_filtered = data.filter(x => (x['str_nationality'] == team_par1) | (x['str_nationality'] == team_par2)).filter(x => filtered_positions.includes(x['str_best_position']))
-    }
-    console.log('here')
-    console.log(data_filtered)
+  d3.json("data/teams_basic_skills.json")
+    .then(function (data) {
+      var data_filtered
+      if (position_filter == 'no_filter') {
+        data_filtered = data.filter(x => (x['str_nationality'] == team_par1) | (x['str_nationality'] == team_par2))
+      } else {
+        const filtered_positions = return_valid_positions(position_filter)
+        console.log(filtered_positions)
+        data_filtered = data.filter(x => (x['str_nationality'] == team_par1) | (x['str_nationality'] == team_par2)).filter(x => filtered_positions.includes(x['str_best_position']))
+      }
+      console.log('here')
+      console.log(data_filtered)
 
-    data_filtered.forEach(function (d) {
-      dimensions.forEach(function (p) {
-        d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
+      data_filtered.forEach(function (d) {
+        dimensions.forEach(function (p) {
+          d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
+        });
+
+        // truncate long text strings to fit in data table
+        for (var key in d) {
+          if (d[key] && d[key].length > 35) d[key] = d[key].slice(0, 36);
+        }
       });
 
-      // truncate long text strings to fit in data table
-      for (var key in d) {
-        if (d[key] && d[key].length > 35) d[key] = d[key].slice(0, 36);
-      }
-    });
-
-    // type/dimension default setting happens here
-    dimensions.forEach(function (dim) {
-      if (!("domain" in dim)) {
-        // detect domain using dimension type's extent function
-        if (dim.description == team_par1) {
-          dim.domain = d3_functor(dim.type.extent)(data_filtered.filter(x => (x['str_nationality'] == team_par1))
-            .map(function (d) { return d[dim.key]; }));
-        }
-        else if (dim.description == team_par2) {
-          dim.domain = d3_functor(dim.type.extent)(data_filtered.filter(x => (x['str_nationality'] == team_par2))
-            .map(function (d) { return d[dim.key]; }));
-        }
-        else {
-          dim.domain = d3_functor(dim.type.extent)(data_filtered.map(function (d) { return d[dim.key]; }));
-        }
-
-      }
-      if (!("scale" in dim)) {
-        // use type's default scale for dimension
-        dim.scale = dim.type.defaultScale.copy();
-      }
-      dim.scale.domain(dim.domain);
-    });
-
-    var render = renderQueue(draw).rate(5);
-    if (canvas.node()) {
-
-      ctx.clearRect(0, 0, width, height);
-      ctx.globalAlpha = d3.min([1.15 / Math.pow(data_filtered.length, 0.25), 1]);
-    }
-    render(data_filtered);
-
-    axes.append("g")
-      .each(function (d) {
-        if (d.description == team_par2) {
-          var renderAxis = "axis" in d
-            ? d.axis.scale(d.scale)  // custom axis
-            : yAxisRight.scale(d.scale);  // default axis
-          d3.select(this).call(renderAxis);
-        } else {
-          var renderAxis = "axis" in d
-            ? d.axis.scale(d.scale)  // custom axis
-            : yAxisLeft.scale(d.scale);  // default axis
-          d3.select(this).call(renderAxis);
-        }
-      })
-      .append("text")
-      .attr("class", "title")
-      .attr("text-anchor", "start")
-      .text(function (d) { return "description" in d ? d.description : d.key; });
-
-    // Add and store a brush for each axis.
-    axes.append("g")
-      .attr("class", "brush")
-      .each(function (d) {
-        d3.select(this).call(d.brush = d3.brushY()
-          .extent([[-10, 0], [10, height]])
-          .on("start", brushstart)
-          .on("brush", brush)
-          .on("end", brush)
-        )
-      })
-      .selectAll("rect")
-      .attr("x", -8)
-      .attr("width", 16);
-
-    d3.selectAll(".axis.Name.tick text")
-      .style("fill", color);
-
-
-    function project(d) {
-      return dimensions.map(function (p, i) {
-        // check if data element has property and contains a value
-        if (
-          !(p.key in d) ||
-          d[p.key] === null
-        ) return null;
-
-        return [xscale(i), p.scale(d[p.key])];
-      });
-    };
-
-    function draw(d) {
-      if (!canvas.node())
-        return
-
-      ctx.strokeStyle = color(d.str_nationality);
-      ctx.lineWidth = 3;
-      ctx.beginPath();
-      var coords = project(d);
-      coords.forEach(function (p, i) {
-        // this tricky bit avoids rendering null values as 0
-        if (p === null) {
-          // this bit renders horizontal lines on the previous/next
-          // dimensions, so that sandwiched null values are visible
-          if (i > 0) {
-            var prev = coords[i - 1];
-            if (prev !== null) {
-              ctx.moveTo(prev[0], prev[1]);
-              ctx.lineTo(prev[0] + 6, prev[1]);
-            }
+      // type/dimension default setting happens here
+      dimensions.forEach(function (dim) {
+        if (!("domain" in dim)) {
+          // detect domain using dimension type's extent function
+          if (dim.description == team_par1) {
+            dim.domain = d3_functor(dim.type.extent)(data_filtered.filter(x => (x['str_nationality'] == team_par1))
+              .map(function (d) { return d[dim.key]; }));
           }
-          if (i < coords.length - 1) {
-            var next = coords[i + 1];
-            if (next !== null) {
-              ctx.moveTo(next[0] - 6, next[1]);
-            }
+          else if (dim.description == team_par2) {
+            dim.domain = d3_functor(dim.type.extent)(data_filtered.filter(x => (x['str_nationality'] == team_par2))
+              .map(function (d) { return d[dim.key]; }));
           }
-          return;
-        }
+          else {
+            dim.domain = d3_functor(dim.type.extent)(data_filtered.map(function (d) { return d[dim.key]; }));
+          }
 
-        if (i == 0) {
-          ctx.moveTo(p[0], p[1]);
-          return;
         }
-
-        ctx.lineTo(p[0], p[1]);
+        if (!("scale" in dim)) {
+          // use type's default scale for dimension
+          dim.scale = dim.type.defaultScale.copy();
+        }
+        dim.scale.domain(dim.domain);
       });
-      ctx.stroke();
-    };
 
-    function brushstart() {
-      d3.event.sourceEvent.stopPropagation();
-    }
+      var render = renderQueue(draw).rate(5);
+      if (canvas.node()) {
 
-    // Handles a brush event, toggling the display of foreground lines.
-    function brush() {
-      render.invalidate();
+        ctx.clearRect(0, 0, width, height);
+        ctx.globalAlpha = d3.min([1.15 / Math.pow(data_filtered.length, 0.25), 1]);
+      }
+      render(data_filtered);
 
-      var actives = [];
-      svg.selectAll(".axis .brush")
-        .filter(function (d) {
-          return d3.brushSelection(this);
-        })
+      axes.append("g")
         .each(function (d) {
-          actives.push({
-            dimension: d,
-            extent: d3.brushSelection(this)
-          });
-        });
-      var selected = data_filtered.filter(function (d) {
-        if (actives.every(function (active) {
-          var dim = active.dimension;
-          if ((dim.description == team_par1) & (d['str_nationality'] == team_par2)) {
-            return true
+          if (d.description == team_par2) {
+            var renderAxis = "axis" in d
+              ? d.axis.scale(d.scale)  // custom axis
+              : yAxisRight.scale(d.scale);  // default axis
+            d3.select(this).call(renderAxis);
+          } else {
+            var renderAxis = "axis" in d
+              ? d.axis.scale(d.scale)  // custom axis
+              : yAxisLeft.scale(d.scale);  // default axis
+            d3.select(this).call(renderAxis);
           }
-          if ((dim.description == team_par2) & (d['str_nationality'] == team_par1)) {
-            return true
-          }
-          // test if point is within extents for each active brush
-          return dim.type.within(d[dim.key], active.extent, dim);
-        })) {
-          return true;
-        }
-      });
-
-      // show ticks for active brush dimensions
-      // and filter ticks to only those within brush extents
-
-      svg.selectAll(".axis")
-        .filter(function (d) {
-          return actives.indexOf(d) > -1 ? true : false;
         })
-        .classed("active_brush", true)
-        .each(function (dimension, i) {
-          var extent = extents[i];
-          d3.select(this)
-            .selectAll(".tick text")
-            .style("display", function (d) {
-              var value = dimension.type.coerce(d);
-              return dimension.type.within(value, extent, dimension) ? null : "none";
+        .append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "start")
+        .text(function (d) { return "description" in d ? d.description : d.key; });
+
+      // Add and store a brush for each axis.
+      axes.append("g")
+        .attr("class", "brush")
+        .each(function (d) {
+          d3.select(this).call(d.brush = d3.brushY()
+            .extent([[-10, 0], [10, height]])
+            .on("start", brushstart)
+            .on("brush", brush)
+            .on("end", brush)
+          )
+        })
+        .selectAll("rect")
+        .attr("x", -8)
+        .attr("width", 16);
+
+      d3.selectAll(".axis.Name.tick text")
+        .style("fill", color);
+
+
+      function project(d) {
+        return dimensions.map(function (p, i) {
+          // check if data element has property and contains a value
+          if (
+            !(p.key in d) ||
+            d[p.key] === null
+          ) return null;
+
+          return [xscale(i), p.scale(d[p.key])];
+        });
+      };
+
+      function draw(d) {
+        if (!canvas.node())
+          return
+
+        ctx.strokeStyle = color(d.str_nationality);
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        var coords = project(d);
+        coords.forEach(function (p, i) {
+          // this tricky bit avoids rendering null values as 0
+          if (p === null) {
+            // this bit renders horizontal lines on the previous/next
+            // dimensions, so that sandwiched null values are visible
+            if (i > 0) {
+              var prev = coords[i - 1];
+              if (prev !== null) {
+                ctx.moveTo(prev[0], prev[1]);
+                ctx.lineTo(prev[0] + 6, prev[1]);
+              }
+            }
+            if (i < coords.length - 1) {
+              var next = coords[i + 1];
+              if (next !== null) {
+                ctx.moveTo(next[0] - 6, next[1]);
+              }
+            }
+            return;
+          }
+
+          if (i == 0) {
+            ctx.moveTo(p[0], p[1]);
+            return;
+          }
+
+          ctx.lineTo(p[0], p[1]);
+        });
+        ctx.stroke();
+      };
+
+      function brushstart() {
+        d3.event.sourceEvent.stopPropagation();
+      }
+
+      // Handles a brush event, toggling the display of foreground lines.
+      function brush() {
+        render.invalidate();
+
+        var actives = [];
+        svg.selectAll(".axis .brush")
+          .filter(function (d) {
+            return d3.brushSelection(this);
+          })
+          .each(function (d) {
+            actives.push({
+              dimension: d,
+              extent: d3.brushSelection(this)
             });
+          });
+        var selected = data_filtered.filter(function (d) {
+          if (actives.every(function (active) {
+            var dim = active.dimension;
+            if ((dim.description == team_par1) & (d['str_nationality'] == team_par2)) {
+              return true
+            }
+            if ((dim.description == team_par2) & (d['str_nationality'] == team_par1)) {
+              return true
+            }
+            // test if point is within extents for each active brush
+            return dim.type.within(d[dim.key], active.extent, dim);
+          })) {
+            return true;
+          }
         });
 
-      // reset dimensions without active brushes
-      svg.selectAll(".axis")
-        .filter(function (d) {
-          return actives.indexOf(d) > -1 ? false : true;
-        })
-        .classed("active_brush", false)
-        .selectAll(".tick text")
-        .style("display", null);
+        // show ticks for active brush dimensions
+        // and filter ticks to only those within brush extents
+
+        svg.selectAll(".axis")
+          .filter(function (d) {
+            return actives.indexOf(d) > -1 ? true : false;
+          })
+          .classed("active_brush", true)
+          .each(function (dimension, i) {
+            var extent = extents[i];
+            d3.select(this)
+              .selectAll(".tick text")
+              .style("display", function (d) {
+                var value = dimension.type.coerce(d);
+                return dimension.type.within(value, extent, dimension) ? null : "none";
+              });
+          });
+
+        // reset dimensions without active brushes
+        svg.selectAll(".axis")
+          .filter(function (d) {
+            return actives.indexOf(d) > -1 ? false : true;
+          })
+          .classed("active_brush", false)
+          .selectAll(".tick text")
+          .style("display", null);
 
 
-      ctx.clearRect(0, 0, width, height);
-      ctx.globalAlpha = d3.min([0.85 / Math.pow(selected.length, 0.3), 1]);
-      render(selected);
+        ctx.clearRect(0, 0, width, height);
+        ctx.globalAlpha = d3.min([0.85 / Math.pow(selected.length, 0.3), 1]);
+        render(selected);
 
-    };
+      };
 
 
-  });
+    });
 }
 
 function return_valid_positions(position_filter) {
